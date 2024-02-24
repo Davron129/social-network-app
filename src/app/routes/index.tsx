f7da870e9aeca029f4dd1d3c148760aeba236829
davron129 <normominovdavron001@gmail.com>
2024-02-24 18:47:46 +0500

    feat: all routes added

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,9 +1,17 @@
 import { RouteObject } from 'react-router-dom';
+import {
+  ROUTE_CONTACTS,
+  ROUTE_CONTACTS_VIEW,
+  ROUTE_HOME,
+  ROUTE_SETTINGS,
+  ROUTE_SETTINGS_PROFILE,
+} from '@shared/constants';
+
 import { HomePage } from '@pages/home';
+import { ProfilePage } from '@pages/settings';
 import { ContactsPage } from '@pages/contacts';
 import { SelectChatScreen } from '@shared/screens';
-import { ContactsLayout, MainLayout } from '@shared/layouts';
-import { ROUTE_CONTACTS, ROUTE_CONTACTS_VIEW, ROUTE_HOME } from '@shared/constants';
+import { ContactsLayout, MainLayout, SettingsLayout } from '@shared/layouts';
 
 export const routes: RouteObject[] = [
   {
@@ -25,6 +33,20 @@ export const routes: RouteObject[] = [
           {
             path: ROUTE_CONTACTS_VIEW,
             element: <ContactsPage />,
+          },
+        ],
+      },
+      {
+        path: ROUTE_SETTINGS,
+        element: <SettingsLayout />,
+        children: [
+          {
+            index: true,
+            element: <SelectChatScreen />,
+          },
+          {
+            path: ROUTE_SETTINGS_PROFILE,
+            element: <ProfilePage />,
           },
         ],
       },
