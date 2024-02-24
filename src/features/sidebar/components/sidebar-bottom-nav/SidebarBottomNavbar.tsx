fdad14c6f19a267d140d4ed4c3fa8b4e8fc2539a
davron129 <normominovdavron001@gmail.com>
2024-02-24 18:56:38 +0500

    feat: sidebar bottom navbar change style on active

--- src/features/sidebar/components/sidebar-bottom-nav/SidebarBottomNavbar.tsx
+++ src/features/sidebar/components/sidebar-bottom-nav/SidebarBottomNavbar.tsx
@@ -1,14 +1,23 @@
+import clsx from 'clsx';
+import { NavLink } from 'react-router-dom';
 import { HiCog, HiUserCircle } from 'react-icons/hi2';
+import { ROUTE_CONTACTS, ROUTE_SETTINGS } from '@shared/constants';
 
 export const SidebarBottomNavbar = () => {
   return (
     <div className="h-12 border-t border-secondary bg-primary grid grid-cols-2">
-      <div className="col-span-1 flex items-center justify-center">
-        <HiUserCircle className="text-muted text-3xl" />
-      </div>
-      <div className="col-span-1 flex items-center justify-center">
-        <HiCog className="text-muted text-3xl" />
-      </div>
+      <NavLink to={ROUTE_CONTACTS} className="col-span-1 flex items-center justify-center">
+        {({ isActive }) => (
+          <HiUserCircle
+            className={clsx('text-3xl', isActive ? 'text-blue-primary' : 'text-muted')}
+          />
+        )}
+      </NavLink>
+      <NavLink to={ROUTE_SETTINGS} className="col-span-1 flex items-center justify-center">
+        {({ isActive }) => (
+          <HiCog className={clsx('text-3xl', isActive ? 'text-blue-primary' : 'text-muted')} />
+        )}
+      </NavLink>
     </div>
   );
 };
