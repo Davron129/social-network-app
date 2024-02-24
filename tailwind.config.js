ff7b5543b28b9105d54110ffaf86387640595cc1
davron129 <normominovdavron001@gmail.com>
2024-02-24 13:47:36 +0500

    feat: tailwind colors extend

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#19212D",
+        "chat-active": "#456897",
+        white: "#FFFFFF",
+        muted: "#B3C2D5",
+        secondary: "#233040",
+        dark: "#131415",
+        "blue-primary": "#4EA2FE",
+        "blue-secondary": "#72BAF8",
+      },
+    },
   },
   plugins: [],
 };
