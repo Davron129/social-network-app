db9d2a08b2dc22bfd4b936eb953b92a78183803b
davron129 <normominovdavron001@gmail.com>
2024-02-26 22:29:31 +0500

    fix: email validation on profile schema

--- src/features/profile/schemas/profile.schema.ts
+++ src/features/profile/schemas/profile.schema.ts
@@ -5,7 +5,12 @@ import {
   PROFILE_FIELD_NAME,
   PROFILE_FIELD_PROFILE_IMG,
 } from '../constants';
-import { MSG_INVALID_EMAIL, MSG_MAX_LENGTH, MSG_REQUIRED_FIELD } from '@shared/constants';
+import {
+  EMAIL_REGEX,
+  MSG_INVALID_EMAIL,
+  MSG_MAX_LENGTH,
+  MSG_REQUIRED_FIELD,
+} from '@shared/constants';
 
 export const ProfileSchema = yup.object().shape({
   [PROFILE_FIELD_NAME]: yup.string().required(MSG_REQUIRED_FIELD(PROFILE_FIELD_NAME)),
@@ -15,7 +20,7 @@ export const ProfileSchema = yup.object().shape({
     .required(MSG_REQUIRED_FIELD(PROFILE_FIELD_BIO)),
   [PROFILE_FIELD_EMAIL]: yup
     .string()
-    .email(MSG_INVALID_EMAIL)
-    .required(MSG_REQUIRED_FIELD(PROFILE_FIELD_EMAIL)),
+    .required(MSG_REQUIRED_FIELD(PROFILE_FIELD_EMAIL))
+    .matches(EMAIL_REGEX, MSG_INVALID_EMAIL),
   [PROFILE_FIELD_PROFILE_IMG]: yup.string().required(MSG_REQUIRED_FIELD(PROFILE_FIELD_PROFILE_IMG)),
 });
--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,4 +1,5 @@
 export * from './base.constants';
 export * from './msg.constants';
 export * from './query-key.constants';
+export * from './regex-pattern.constants';
 export * from './route.constants';
