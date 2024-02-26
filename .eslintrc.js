563080e787bb788d21ba3ad6f04eba11bc6eb423
davron129 <normominovdavron001@gmail.com>
2024-02-26 21:53:58 +0500

    feat: jest configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react/recommended',
+    // 'plugin:import/recommended'
   ],
   overrides: [
     {
@@ -29,5 +31,6 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/jsx-uses-react': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 0,
   },
 };
--- craco.config.js
+++ craco.config.js
@@ -1,11 +1,30 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      "@app": path.resolve(__dirname, "src/app"),
-      "@pages": path.resolve(__dirname, "src/pages"),
-      "@shared": path.resolve(__dirname, "src/shared"),
-      "@features": path.resolve(__dirname, "src/features"),
+      '@app': path.resolve(__dirname, 'src/app'),
+      '@pages': path.resolve(__dirname, 'src/pages'),
+      '@shared': path.resolve(__dirname, 'src/shared'),
+      '@features': path.resolve(__dirname, 'src/features'),
+    },
+  },
+  jest: {
+    configure: {
+      verbose: true,
+      moduleNameMapper: {
+        '^@app/(.*)$': '<rootDir>/src/app/$1',
+        '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+        '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+        '^@features/(.*)$': '<rootDir>/src/features/$1',
+        '^axios$': 'axios/dist/node/axios.cjs',
+      },
+    },
+    modulePaths: ['<rootDir>'],
+    preset: 'ts-jest',
+    testEnvironment: 'jest-environment-jsdom',
+    transform: {
+      '^.+\\.tsx?$': 'ts-jest',
+      // process `*.tsx` files with `ts-jest`
     },
   },
 };
